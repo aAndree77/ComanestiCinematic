7f268655c8cb37dc46d21db3f8f987b0dbc2d14f
Insuratalu Andrei Vladimir <andreiinsuratalu87@gmail.com>
2026-03-31 20:48:53 +0300

    Update HeroBanner.js

--- app/components/HeroBanner.js
+++ app/components/HeroBanner.js
@@ -8,7 +8,7 @@ export default function HeroBanner() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover opacity-60"
       >
-        <source src="/RECLAMA.MP4" type="video/mp4" />
+        <source src="/RECLAMA.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
